ec4f44ec9e67c945af92f488ace9a7e12c4cccdd
Mary <omota4fidelity@gmail.com>
2023-10-06 11:25:27 +0100

    completed

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, discount = 0):
+    self.discount =  discount
+    self.total = 0
+    self.items = []
+    pass
+
+  def total(self):
+    return self.total
+  
+  def item (self):
+    return self.item
+  
+  def title_price(self, title, price):
+    if (type(title) in str) and (type(price) in (int, float)):
+      self.total += price
+    else:
+      pass
